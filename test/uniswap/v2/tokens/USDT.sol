568635772d1e7d924adf54c6f0c87efbc591b013
biyou <3071709373@qq.com>
2024-11-13 15:49:57 +0800

    Fix lib

---
+++ test/uniswap/v2/tokens/USDT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDT is Ownable, ERC20 {
+    constructor() Ownable(msg.sender) ERC20("USDT", "USDT") {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
